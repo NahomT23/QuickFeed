da8f585d674328b56bb47fce476a501788388bf9
Nahom Tsegaye <tnahom214@gmail.com>
2024-12-01 23:24:51 +0300

    gif added

--- app/(user)/projects/[projectId]/instructions/page.tsx
+++ app/(user)/projects/[projectId]/instructions/page.tsx
@@ -1,31 +1,3 @@
-// import React from 'react'
-// import  CopyPasteBtn  from '@/components/CopyPasteBtn'
-// const Page = ({ params }: { params: {
-//     projectId: string
-// }}) => {
-//     if(!params.projectId) return (<div>Invalid Project Id</div>)
-//     // if(!process.env.WIDGET_URL) return (<div>Missing widget url</div>)
-//   return (
-//     <div>
-//         <h1 className='text-xl font-bold mb-2'>
-//             Start Collecting <strong>FeedBack</strong>
-//         </h1>
-//         <p className='text-lg text-secondary-foreground'>
-//             Embed the code in your site
-//         </p>
-//         <div className='bg-blue-950 p-3 rounded-md mt-6 relative'>
-//         <code className='text-white'>
-//             {`<my-widget project-id="${params.projectId}"></my-widget>`}
-//             <br />
-//             {`<script src="${process.env.WIDGET_URL}/widget.umd.js"></script>`}
-//         </code>
-//         <CopyPasteBtn text={`<my-widget project="${params.projectId}"></my-widget> \n <script src="${process.env.WIDGET_URL}/widget.umd.js"></script>`}/>
-//         </div>
-//     </div>
-//   )
-// }
-
-// export default Page
 import  CopyPasteBtn  from '@/components/CopyPasteBtn'
 
 const page = ({ params }: {
@@ -44,9 +16,9 @@ const page = ({ params }: {
         <code className=" text-white">
           {`<my-widget project-id="${params.projectId}"></my-widget>`}
           <br />
-          {`<script src="${process.env.WIDGET_URL}widget.umd.js"></script>`}
+          {`<script src="${process.env.WIDGET_URL}/widget.umd.js"></script>`}
         </code>
-        <CopyPasteBtn text={`<my-widget project-id="${params.projectId}"></my-widget>\n<script src="${process.env.WIDGET_URL}widget.umd.js"></script>`} />
+        <CopyPasteBtn text={`<my-widget project-id="${params.projectId}"></my-widget>\n<script src="${process.env.WIDGET_URL}/widget.umd.js"></script>`} />
       </div>
     </div>
   )
